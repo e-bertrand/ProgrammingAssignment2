c597567b6c1670538b6e1fcabd6203c4074be7da
e-bertrand <ebertrand@gmail.com>
2015-11-15 00:44:11 +0100

    Complete solution for PA2
    Full development of the functions included in Programming Assigment 2
    ("R Programming" course /JHU)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for computing the inverse of a square matrix and caching the
+## object as it is a time-consuming computation. Next time the inverse 
+## of the same matrix is requested, cached data is got
 
-## Write a short comment describing this function
+## Constructor for storing and caching a square matrix object and its 
+## inverse, and defining the basic get/set functions of the object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	
+	## reset inverse
+    inv <- NULL
+    
+    ## set the matrix stored and reset its inverse
+    set <- function(y = matrix()) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    ## get the stored matrix
+    get <- function() x
+    
+    ## set / get the inverse of the stored matrix
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    
+    ## list the basic get/set functions for the matrix object
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+		 
 }
 
 
-## Write a short comment describing this function
+## Function for computing the inverse a stored matrix (new matrix) 
+## or getting the inverse from the cache (existing matrix)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## get the cached inverse (if it exists)
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached inversion")
+        return(inv)
+    }
+    
+    ## if the cached inverse doesn't exists, getting the stored matrix and
+    ## computing, caching and returning the inverse
+    data <- x$get()
+    ## As PA2 stated, it is assumed that it is a square matrix and
+    ## its inverse is computable
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
+	
 }
